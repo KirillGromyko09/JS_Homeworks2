cd3b0f18539a4a98fd49983daf0ec9f0af91480c
Kirill Gromyko <goldgrom01@gmail.com>
2024-02-25 19:40:14 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+// Task 1
+const admin = 'John';
+const name = 'John';
+alert(admin)
+
+// Task 2
+const planet = "Earth";
+const userName = "John Doe";
+
+// Task 3
+let num = 10 + "10";
+console.log(num)
+console.log(typeof num)
