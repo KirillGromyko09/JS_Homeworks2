15e7f227247ccce36beee3185d78ad529787e7a2
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-02-26 15:36:34 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 'use strict';
+// // Task 1
+let number1 = prompt('Any number');
+let number2 = prompt('Any number');
+let number3 = prompt('Any number');
+alert(number1 + number2 + number3);
+
+// Task 2
+alert('1'+' '+'4'+' '+'8'+' '+'5'+' '+'9');
