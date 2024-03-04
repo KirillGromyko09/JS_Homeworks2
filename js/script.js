646dfa7a7258effb0a2e434a1044efde45423850
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-03-04 14:33:35 +0200

    completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 'use strict';
+const temp= +prompt('Please specify the temperature');
+const type = prompt('Specify the unit of measurement where F is Fahrenheit and C is Celsium');
+
+if (!isNaN(temp)) {
+    let result = null;
+
+    if (type.toUpperCase() === 'F') {
+        result = ('It will be ') + (temp - 32) * 9 / 5 +(' degrees Fahrenheit');
+    } else if (type.toUpperCase() === 'C') {
+        result = ('It will be ') + ((temp * 9 / 5) + 32) + (' degrees Celsium');
+    } else {
+        result = ('Error , check the entered data and try again ');
+    }
+    alert(result);
+} else {
+    alert('Wrong number!');
+}
