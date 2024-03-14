e0427155bf2b0de225873137f60544d72c845c03
Kirill Gromyko <goldgrom01@gmail.com>
2024-03-14 18:53:02 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 'use strict';
+// Task 1
+let user = {}
+user.name = 'John';
+user.surname= 'Smith';
+user.name= 'Pete';
+delete user.name;
+
+// Task 2
+const user2 = {
+    name: 'John'
+};
+user2.name = 'Pete';
+// Так,це буде прациювати, оскільки const обмежує зміну посилання на обєкт , а не його властивостей
+
+// Task 3
+const salaries = {
+    John: 100,
+    Ann: 160,
+    Pete: 130
+}
+
+const sum = salaries.John + salaries.Ann + salaries.Pete;
+console.log(sum);
