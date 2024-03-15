c595d0c35a62ee8c15c7ade7950dddca57431347
Kirill Gromyko <goldgrom01@gmail.com>
2024-03-15 15:59:02 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
 'use strict';
+const operation = prompt('Що ви хочете зробити? (add,sub,mult,div)');
+const operand1= +prompt('Введіть перше число');
+const operand2 = +prompt('Введіть друге число');
+
+let result
+let operationSymbol
+switch (operation) {
+    case 'add':
+        result=operand1 + operand2;
+        operationSymbol = '+';
+        break;
+
+    case 'sub':
+        result = operand1 - operand2;
+        operationSymbol = '-';
+        break;
+    case 'mult':
+        result = operand1 * operand2;
+        operationSymbol = '*';
+        break;
+    case 'div':
+        result=operand1 / operand2;
+        operationSymbol = '/';
+        break;
+    default:
+        alert('Невідома операція')
+}
+if (operationSymbol){
+    console.log(`${operand1} ${operationSymbol} ${operand2} = ${result}`);
+}
