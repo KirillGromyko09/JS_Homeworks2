b743a7d34664296eb7c4ef78214a858c52c4f8d9
Kirill Gromyko <goldgrom01@gmail.com>
2024-03-15 16:55:01 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
 'use strict';
+const operand1= +prompt('Введіть перше число');
+const operand2= +prompt('Введіть друге число');
+const operand3= +prompt('Введіть трєте число');
+const result = (operand1 + operand2 + operand3) / (3);
+alert(result);
