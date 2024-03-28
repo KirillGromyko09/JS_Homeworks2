916a76353ce112180857be2113796ef014715844
Kirill Gromyko <goldgrom01@gmail.com>
2024-03-28 14:31:19 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+const num = +prompt('Введіть число');
+
+if (num<1 || isNaN(num)){
+    console.log('NaN')
+}else{
+    let divisor=2;
+
+    while(num % divisor !== 0){
+        divisor++
+    }
+    console.log('Найменший дільник ' + divisor)
+}
