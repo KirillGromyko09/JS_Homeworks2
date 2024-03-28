7e8ae178a2b29672cc24f2be887e61953bea199d
Kirill Gromyko <goldgrom01@gmail.com>
2024-03-28 14:49:52 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
 'use strict';
+let sum = 0;
+
+outerLoop:
+    for (let i = 1; i <= 20; i++) {
+        if (i % 2 === 0) {
+            continue;
+        } else {
+            sum += i;
+        }
+    }
+
+console.log(`Сума всіх непарних чисел в діапазоні від 1 до 20: ${sum}`);
