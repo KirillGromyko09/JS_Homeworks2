df4a7c35b22a4a559f8bea92db2bdcec818e2835
Kirill Gromyko <goldgrom01@gmail.com>
2024-03-28 19:58:58 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,21 @@
 'use strict';
+'use strict';
+
+const userAgeInput = +prompt("Введіть ваш вік");
+
+if (!isNaN(userAgeInput) && userAgeInput !== null) {
+    const age = parseInt(userAgeInput);
+
+    let yearsWord;
+    if (age % 10 === 1 && age % 100 !== 11) {
+        yearsWord = "рік";
+    } else if (age % 10 >= 2 && age % 10 <= 4 && (age % 100 < 10 || age % 100 >= 20)) {
+        yearsWord = "роки";
+    } else {
+        yearsWord = "років";
+    }
+
+    alert("Вам " + age + " " + yearsWord);
+} else {
+    alert("Введіть ваш вік, будь ласка");
+}
