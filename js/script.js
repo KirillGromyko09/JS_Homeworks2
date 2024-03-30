1ef513458de67ded74b1ed07852ed7c12687b1bf
Kirill Gromyko <goldgrom01@gmail.com>
2024-03-30 15:14:08 +0200

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,15 @@
 'use strict';
+const number= +prompt('Введіть число');
+const degreeOfNumber = +prompt('Введіть ступінь числа');
+
+function numberDegree (number,degreeOfNumber = 1){
+    if (isNaN(number) || isNaN(degreeOfNumber)){
+        alert('Введіть правильні значення');
+        return;
+    }
+    return Math.pow(number,degreeOfNumber);
+}
+const result = numberDegree(number,degreeOfNumber);
+if (result !== undefined){
+    alert('Результат '+ result);
+}
