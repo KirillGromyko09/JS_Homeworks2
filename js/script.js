357e6200034a383650c1cd66e85e12473f795351
Kirill Gromyko <goldgrom01@gmail.com>
2024-04-02 17:25:00 +0300

    init

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
 'use strict';
+const numbers = [1,2,3,-1,-2,-3];
+const positiveNumbers = function (arr){
+    const result = [];
+    if (!arr.length) return 'empty'
+
+    for (let i = 0; i < arr.length; i++){
+        const isPositive = Math.sign(arr[i]) === 1 || Math.sign(arr[i]) === 0 ;
+        isPositive && result.push(arr[i]);
+    }
+
+    return result.length ? result : null
+}
+console.log(positiveNumbers(numbers));
