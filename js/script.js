2ace92a28046f2654f8c9a64ab6bcddaa826d451
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-04-17 15:06:57 +0300

    completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-'use strict';
+function factorial(n) {
+  if (n === 1 || n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+console.log(factorial(3));
+
+function pow(num, degree) {
+  if (degree === 1) {
+    return num;
+  }
+  return num * pow(num, degree - 1);
+}
+
+console.log(pow(2, 10));
+
+function sum(a, b) {
+  if (b === 0) {
+    return a;
+  }
+  return sum(a + 1, b - 1);
+}
+
+console.log(sum(31, 4));
