cade6d68725f26485700b2cfb5d8f3e5fced3725
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-05-01 14:26:11 +0300

    completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-'use strict';
+const user1 = {
+  name: 'John',
+  lastName: 'Kramer',
+  age: 54,
+};
+
+const user2 = {
+  name: 'Sasha',
+  lastName: 'Doe',
+  age: 31,
+};
+
+const getFullName = function (prefix = 'Mr') {
+  return `${prefix} ${this.name} ${this.lastName}`;
+};
+const user1Full = getFullName.bind(user1, 'Mr.');
+console.log(user1Full());
+
+const getFullName2 = function (prefix = 'Mr') {
+  return `${prefix} ${this.name} ${this.lastName}`;
+};
+const userFullName2 = getFullName2.bind(user2, 'Mr.');
+console.log(userFullName2());
