5a682ca2238f89f413722e4a5b80697f4f3c48a0
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-05-02 16:16:07 +0300

    completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
-'use strict';
+(function () {
+  const sum = function (param) {
+    let total = param;
+    return function (num) {
+      total += num;
+      return total;
+    };
+  };
+  const result = sum(0);
+  console.log(result(3));
+  console.log(result(5));
+  console.log(result(20));
+}());
