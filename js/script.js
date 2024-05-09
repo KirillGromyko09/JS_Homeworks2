7cb52e07f92c60e63d8b42d762f476ce8b895e27
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-05-09 16:20:05 +0300

    completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
-'use strict';
+const ul = document.body.firstElementChild;
+const arrNameAttr = [];
+const arrValue = [];
+for (const attributes of ul.attributes) {
+  arrNameAttr.push(attributes);
+  arrValue.push(attributes.nodeValue);
+}
+console.log(arrNameAttr);
+console.log(arrValue);
+ul.lastElementChild.innerHTML = 'Hello, my name is Kirill';
+ul.firstElementChild.setAttribute('data-my-name', 'Kirill');
+ul.removeAttribute('data-dog-tail');
