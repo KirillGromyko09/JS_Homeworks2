fa3ee6c6a2dd250d6697ce71df2e6bdd767a2863
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-05-09 14:01:36 +0300

    completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-'use strict';
+const ulList = document.getElementById('ulId');
+const liItems = document.getElementsByTagName('li');
+
+const arr = [];
+for (const element of liItems) {
+  if (element instanceof HTMLElement) arr.push(element);
+}
+console.log(arr);
+console.log(arr.length);
+
+const textArray = [];
+for (const item of liItems) {
+  textArray.push(item.textContent);
+}
+
+console.log(textArray);
