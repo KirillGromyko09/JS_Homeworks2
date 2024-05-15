8e87b5db5189fef75a22740024403235c6596441
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-05-15 15:37:58 +0300

    completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,23 @@
-'use strict';
+(function () {
+  const arr = [1, 2, [1.1, 1.2, 1.3], 3];
+  const generateList = (array) => {
+    const ulList = document.createElement('ul');
+
+    array.forEach((item) => {
+      const listItem = document.createElement('li');
+
+      if (Array.isArray(item)) {
+        listItem.appendChild(generateList(item));
+      } else {
+        const textNode = document.createTextNode(item);
+        listItem.appendChild(textNode);
+      }
+
+      ulList.appendChild(listItem);
+    });
+
+    return ulList;
+  };
+
+  console.log(generateList(arr));
+}());
