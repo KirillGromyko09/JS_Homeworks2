4f0ca5749b27b836fa3299ee425dc5503f856e06
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-05-27 15:17:01 +0300

    completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
-'use strict';
+(function () {
+  document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form');
+
+    form.addEventListener('submit', (event) => {
+      event.preventDefault();
+
+      const formData = new FormData(form);
+      const data = {};
+
+      formData.forEach((value, key) => {
+        data[key] = value;
+      });
+
+      localStorage.setItem('formData', JSON.stringify(data));
+
+      console.log('Form Data Saved:', data);
+
+      form.reset();
+
+      window.location.href = 'display.html';
+    });
+  });
+}());
