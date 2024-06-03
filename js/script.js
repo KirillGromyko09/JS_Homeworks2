d470234d8d7e60bdc998d41f6e32c901dea21f7e
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-06-03 19:18:17 +0300

    completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,37 @@
-'use strict';
+(function () {
+  const TODO_ITEMS = 'todo-items';
+  const form = document.querySelector('#todoForm');
+  let currentId = 1;
+
+  const getData = () => {
+    const data = JSON.parse(localStorage.getItem(TODO_ITEMS));
+    return data || [];
+  };
+  const SaveTodoItem = () => {
+    const dataToSave = structuredClone(data);
+    dataToSave.id = currentId;
+    currentId++;
+    getData();
+  };
+
+  const CreateTodoItem = (event) => {
+    event.preventDefault();
+    event.preventDefault();
+
+    const data = {
+      title: null,
+      description: null,
+    };
+    event.target.querySelectorAll('input,textarea').forEach((input) => {
+      data[input.name] = input.value;
+    });
+    const savedItem = SaveTodoItem(data);
+    event.target.reset();
+  };
+
+  const CreateTodoItemLayout = () => {
+
+  };
+
+  form.addEventListener('submit', CreateTodoItem);
+}());
