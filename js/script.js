80c86dd4a6c649e266acda5751df1707d13a0b6c
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-06-11 16:39:41 +0300

    completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-'use strict';
+const arr = [11, 22, 33];
+const getIteratorObj = (itrDataStructure) => {
+  const dataArr = Array.from(itrDataStructure);
+  let currentIndex = 0;
+  return {
+    next() {
+      let done = false;
+      if (currentIndex + 1 > dataArr.length) {
+        return {
+          done: true,
+        };
+      }
+      const result = {
+        value: dataArr[currentIndex],
+        done: false,
+      };
+      currentIndex += 1;
+      return result;
+    },
+  };
+};
+const itr = getIteratorObj(arr);
+console.log(itr.next());
+console.log(itr.next());
+console.log(itr.next());
+console.log(itr.next());
