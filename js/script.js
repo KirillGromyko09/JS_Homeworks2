75ee6b737b01cd377a8dd20258c9691a555a9732
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-06-11 16:44:28 +0300

    completed

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-'use strict';
+const car = {
+  model: 'Mercedes-Benz',
+  year: 2020,
+  color: 'Black',
+};
+
+const anotherCar = {
+  model: 'BMW',
+  year: 2023,
+  color: 'White',
+};
+
+const getFullCarName = (obj, prefix = 'Amazing') => `${prefix} ${obj.color} ${obj.model}`;
+
+const car1Name = getFullCarName(car);
+console.log(car1Name);
+
+const car2Name = getFullCarName(anotherCar, 'Super');
+console.log(car2Name);
