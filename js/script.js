c5ddd7a7d2578529d20223dbf5e52b9e0e472b82
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-06-19 16:50:53 +0300

    completed

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ firstStudent.present();
 console.log(firstStudent.summary());
 console.log(firstStudent);
 
-const secondStudent = new Student('Alice' , 'Fox', 2000);
+const secondStudent = new Student('Alice', 'Fox', 2000);
 secondStudent.present();
 secondStudent.present();
 secondStudent.setGrade(90);
@@ -24,15 +24,14 @@ secondStudent.setGrade(40);
 console.log(secondStudent.summary());
 console.log(secondStudent);
 
-const thirdStudent = new Student('John', 'Kramer' , 1961);
+const thirdStudent = new Student('John', 'Kramer', 1961);
 thirdStudent.present();
 thirdStudent.present();
 thirdStudent.setGrade(90);
-thirdStudent.present()
+thirdStudent.present();
 thirdStudent.present();
 thirdStudent.setGrade(100);
 thirdStudent.present();
 thirdStudent.setGrade(40);
 console.log(thirdStudent.summary());
 console.log(thirdStudent);
-
