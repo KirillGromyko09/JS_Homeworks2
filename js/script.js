ddcfb69d0d55012ec0250c57ab7410d77f0ee034
Kirill Gromyko <148172272+KirillGromyko09@users.noreply.github.com>
2024-06-19 16:50:25 +0300

    completed

--- js/script.js
+++ js/script.js
@@ -1,15 +1,38 @@
 import Student from './Student.js';
 
-const s = new Student('Sergio', 'Leone', 1992);
-s.present();
-s.present();
-s.setGrade(80);
-s.present();
-s.setGrade(100);
-s.present();
-s.setGrade(100);
-s.present();
+const firstStudent = new Student('Sergio', 'Leone', 1992);
+firstStudent.present();
+firstStudent.present();
+firstStudent.setGrade(80);
+firstStudent.present();
+firstStudent.setGrade(100);
+firstStudent.present();
+firstStudent.setGrade(100);
+firstStudent.present();
+console.log(firstStudent.summary());
+console.log(firstStudent);
 
-console.log(s.summary());
+const secondStudent = new Student('Alice' , 'Fox', 2000);
+secondStudent.present();
+secondStudent.present();
+secondStudent.setGrade(90);
+secondStudent.absent();
+secondStudent.present();
+secondStudent.setGrade(100);
+secondStudent.present();
+secondStudent.setGrade(40);
+console.log(secondStudent.summary());
+console.log(secondStudent);
 
-console.log(s);
+const thirdStudent = new Student('John', 'Kramer' , 1961);
+thirdStudent.present();
+thirdStudent.present();
+thirdStudent.setGrade(90);
+thirdStudent.present()
+thirdStudent.present();
+thirdStudent.setGrade(100);
+thirdStudent.present();
+thirdStudent.setGrade(40);
+console.log(thirdStudent.summary());
+console.log(thirdStudent);
+
