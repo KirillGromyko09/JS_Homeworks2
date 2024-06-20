f09835b505b111cf67117287f223ddfd41c44910
Kirill Gromyko <goldgrom01@gmail.com>
2024-06-20 18:08:30 +0300

    completed

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 (function () {
   const TODO_ITEMS = 'todo-items';
   const form = document.querySelector('#todoForm');
+  const todoItemContainer = document.querySelector('#todoItems')
   let currentId = 1;
 
   const getData = () => {
@@ -9,9 +10,12 @@
   };
   const SaveTodoItem = () => {
     const dataToSave = structuredClone(data);
+    const savedData = getData();
     dataToSave.id = currentId;
-    currentId++;
-    getData();
+    currentId += 1;
+    savedData.push(dataToSave);
+    localStorage.setItem(TODO_ITEMS, JSON.stringify(savedData));
+    return getData().at(-1);
   };
 
   const CreateTodoItem = (event) => {
@@ -25,12 +29,27 @@
     event.target.querySelectorAll('input,textarea').forEach((input) => {
       data[input.name] = input.value;
     });
-    const savedItem = SaveTodoItem(data);
-    event.target.reset();
+    try {
+      const savedItem = SaveTodoItem(data);
+      const todoItemHTML = createTodoItemLayout(savedItem)
+      todoItemContainer.prepend(todoItemHTML);
+    } catch (error) {
+      alert(error.message);
+    } finally {
+      event.target.reset();
+    }
   };
 
-  const CreateTodoItemLayout = () => {
-
+  const CreateTodoItemLayout = (data) => {
+const wrapper = document.createElement('div');
+wrapper.className = 'col-4';
+wrapper.setAttribute('data-todo-id' , data.id)
+    wrapper.innerHTML{`<div class="taskWrapper">
+                        <div class="taskHeading">Title</div>
+                        <div class="taskDescription">Task body</div>
+                       </div>`
+    }
+    return wrapper;
   };
 
   form.addEventListener('submit', CreateTodoItem);
