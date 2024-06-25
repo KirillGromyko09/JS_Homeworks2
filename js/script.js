a7973690da014083a537324eaa47f621bbcc2505
Kirill Gromyko <goldgrom01@gmail.com>
2024-06-25 22:54:06 +0300

    completed

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 const getAllData = () => {
   const getData = new Promise((resolve, reject) => {
-    const dataFromServer = fetch('https://jsonplaceholder.typicode.com/');
+    const dataFromServer = fetch('https://jsonplaceholder.typicode.com/todos/1');
     resolve(dataFromServer);
   })
   return getData;
 }
 console.log(getAllData());
+const p = getAllData();
+console.log(p);
