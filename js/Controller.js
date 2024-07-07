cfefb39a9a33c72cb10b59c5f0dac006acb54d2d
Kirill Gromyko <goldgrom01@gmail.com>
2024-07-07 18:03:59 +0300

    completed

--- js/Controller.js
+++ js/Controller.js
@@ -1,4 +1,20 @@
 class Controller {
+  inputSelector = null;
+  inputEl = null;
+  constructor(input) {
+    this.input = input;
+  }
+  setEvents(){
+    this.inputEl.addEventListener('input', this.handleInput)
+  }
 
+  handleInput = ({target}) => {
+    console.log(target.value);
+}
+
+  set input(val){
+    this.inputSelector = val;
+    this.inputEl = document.querySelector(val);
+  }
 }
 export default Controller;
--- js/script.js
+++ js/script.js
@@ -2,13 +2,4 @@ import Model from "./Model.js";
 import View from "./View.js";
 import Controller from "./Controller.js";
 
-const getAllData = () => {
-  const getData = new Promise((resolve, reject) => {
-    const dataFromServer = fetch('https://jsonplaceholder.typicode.com/todos/1');
-    resolve(dataFromServer);
-  })
-  return getData;
-}
-console.log(getAllData());
-const p = getAllData();
-console.log(p);
+new Controller('[data-input]')
